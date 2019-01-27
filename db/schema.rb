fe951f6c3deab2bf1e70fb838aedee92e4345dd3
zkcyt <tmsinksz@gmail.com>
2019-01-27 15:39:48 +0900

    devise, Navi追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190125120740) do
+ActiveRecord::Schema.define(version: 20190127043922) do
 
   create_table "posts", force: :cascade do |t|
     t.text "title"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 20190125120740) do
     t.integer "like_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
